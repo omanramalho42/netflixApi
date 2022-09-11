b9721c272da7b78915c1a9a6c3c7b4261532f72b
omanramalho <omanapple42@hotmail.com>
2022-09-11 09:21:04 -0300

    fix:criando schema de user para tabela no mongodb

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
+const mongoose = require("mongoose");
+
 import { 
   Request, 
   Response, 
   NextFunction 
 } from "express";
-import mongoose from "mongoose";
 
 const app = express();
 app.use(bodyParser.json());
 
 dotenv.config();
 
-const mongooseUrl = 'mongodb+srv://omanramalho:oman120600@cluster0.5w65jqc.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(mongooseUrl)
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('Mongodb connection successful'))
-  .catch((error) => console.log('Ocorreu um erro durante a conexao com o banco de dados: ' + error.message))
+  .catch((error: any) => console.log('Ocorreu um erro durante a conexao com o banco de dados: ' + error.message))
 
 app.listen(process.env.REACT_API_PORT || 3001, () => {
   console.log('[Server 🦇] Running on port: ' + process.env.REACT_API_PORT || 3001);
